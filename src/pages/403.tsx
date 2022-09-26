414d203130733d2649b68bbc6f10f0ac762299a8
Abel Tran <abeltran.develop@gmail.com>
2022-09-26 20:05:37 +0700

    feat: add 403 & 404 page

---
+++ src/pages/403.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, Result } from 'antd';
+import Link from 'next/link';
+
+const BackHomeLink = () => (
+  <Link href={"/"}>
+    <Button type="primary">Trở về trang chính</Button>
+  </Link>
+)
+
+const FourZeroThreePage: React.FC = () => (
+  <Result
+    status="403"
+    title="403"
+    subTitle="Rất tiếc! Bạn không có quyền truy cập vào trang này."
+    extra={<BackHomeLink />}
+  />
+)
+
+export default FourZeroThreePage;
---
+++ src/pages/404.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, Result } from 'antd';
+import Link from 'next/link';
+
+const BackHomeLink = () => (
+  <Link href={"/"}>
+    <Button type="primary">Trở về trang chính</Button>
+  </Link>
+)
+
+const FourZeroFourPage: React.FC = () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="Thành thật xin lỗi, trang này không tồn tại."
+    extra={<BackHomeLink />}
+  />
+)
+
+export default FourZeroFourPage;
