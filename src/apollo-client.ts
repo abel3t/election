4406beb6bc022bafb640aec7e9f7b627b2945bd8
Abel Tran <abeltran.develop@gmail.com>
2025-07-16 02:18:51 +0700

    update ui

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -10,7 +10,6 @@ import {
 import { REFRESH_TOKEN } from './operation/auth.mutation';
 import jwtDecode from 'jwt-decode';
 import { FetchResult } from '@apollo/client/link/core/types';
-import { subscribe } from 'graphql/execution';
 
 const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/graphql`;
 const httpLink = new HttpLink({ uri: API_URL });
@@ -42,65 +41,24 @@ const callRefreshToken = async (email: string, refreshToken: string) => {
 };
 
 const authMiddleware = new ApolloLink((operation, forward) => {
-  const guest = localStorage.getItem('guest');
-  const logIn = localStorage.getItem('logIn');
-
-  if (guest === 'true' || logIn === 'true') {
-    return forward(operation);
-  }
-
-  let token = localStorage.getItem('token');
-  const expiredTime = localStorage.getItem('expiredTime') || '';
-  const refreshToken = localStorage.getItem('refreshToken');
-
-  if (!token && logIn !== 'true') {
-    window.location.href = '/login';
-  }
-
-  if (expiredTime && expiredTime > new Date().toISOString()) {
-    operation.setContext({
+  // Always get the latest token from localStorage
+  const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+  if (token) {
+    operation.setContext(({ headers = {} }) => ({
       headers: {
-        authorization: token ? `Bearer ${token}` : ''
+        ...headers,
+        authorization: `Bearer ${token}`
       }
-    });
-
-    return forward(operation);
+    }));
+  } else {
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        authorization: ''
+      }
+    }));
   }
-
-  return new Observable<FetchResult>((observer) => {
-    if (token && refreshToken) {
-      const payload: any = jwtDecode(token);
-      callRefreshToken(payload.email, refreshToken)
-        .then((data) => {
-          if (data.errors) {
-            console.log(data.errors);
-            window.location.href = '/login';
-          }
-
-          if (data.refreshToken?.token) {
-            token = data.refreshToken?.token;
-            localStorage.setItem('token', `${token}`);
-          }
-
-          const date = new Date();
-          date.setHours(date.getHours() + 23);
-          localStorage.setItem('expiredTime', date.toISOString());
-
-          operation.setContext({
-            headers: {
-              authorization: token ? `Bearer ${token}` : ''
-            }
-          });
-
-          forward(operation).subscribe(observer);
-        })
-        .catch((error) => {
-          localStorage.clear();
-          console.log(error);
-          window.location.href = '/login';
-        });
-    }
-  });
+  return forward(operation);
 });
 
 const apolloClient = new ApolloClient({
